98725a6d9133d64092375d3ffd548008f9d525b6
Eusuph Qolo <profqolo@gmail.com>
2016-07-12 14:13:42 +0200

    first commit

---
+++ src/hackerrank/Solution4Arithmatic.py
@@ -0,0 +1,15 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+totalAdd = 0
+totalSub = 0
+totalMult = 0
+
+a = input()
+b = input()
+totalAdd = (a + b)
+totalSub = (a - b)
+totalMult = (a * b)
+print totalAdd
+print totalSub
+print totalMult
