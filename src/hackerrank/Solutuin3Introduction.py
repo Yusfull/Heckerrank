ec6e07c4921bad5fbf90d582a5914079def7123e
Eusuph Qolo <profqolo@gmail.com>
2016-07-12 10:58:30 +0200

    first commit

---
+++ src/hackerrank/Solutuin3Introduction.py
@@ -0,0 +1,5 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+print "Hello {0} {1}! You have just delved into python.".format(raw_input(), raw_input())
