346c74944919f6ecbc8c74b193e75fcde48f8b87
Eusuph Qolo <profqolo@gmail.com>
2016-07-13 08:09:57 +0200

    first commit

---
+++ src/introduction/Solution6Devision.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+a = (input());
+b = (input());
+print(a//b)
+print(a/ float(b))
